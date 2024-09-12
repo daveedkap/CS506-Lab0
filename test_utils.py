6aff64f9fb84c6b40cf9cc1d95ecf0eaae0f21d8
Abdurkader Abdi <abdurkaderabdi@gmail.com>
2024-09-12 13:12:18 -0400

    add missing code for tests

--- test_utils.py
+++ test_utils.py
@@ -13,19 +13,25 @@ def test_dot_product():
     assert result == 32, f"Expected 32, but got {result}"
     
 def test_cosine_similarity():
-    ### YOUR CODE HERE
+    vector1 = np.array([1, 2, 3])
+    vector2 = np.array([4, 5, 6])
+
+    result = cosine_similarity(vector1, vector2)
     
-    result = ### YOUR CODE HERE
-    
-    expected_result = ### YOUR CODE HERE
+    expected_result = .97463184619
     
     assert np.isclose(result, expected_result), f"Expected {expected_result}, but got {result}"
 
 def test_nearest_neighbor():
-    ### YOUR CODE HERE
+    target_vector = np.array([1, 2, 3])
+    vector_matrix = [
+        np.array([0, 1, 0])
+        np.array([1, 2, 3])
+        np.array([1, 0, 0])
+    ]
     
-    result = ### YOUR CODE HERE
+    result = nearest_neighbor(target_vector, vectors)
     
-    expected_index = ### YOUR CODE HERE
+    expected_index = 1
     
     assert result == expected_index, f"Expected index {expected_index}, but got {result}"
